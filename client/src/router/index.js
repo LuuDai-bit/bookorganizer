acd563daf32c5dd919db6a2a54c907cf4159de34
LuuDai-bit <jojolove198@gmail.com>
2023-12-31 11:20:40 +0700

    Default route to 404 page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
 import Signup from '../views/SignupView.vue'
+import PageNotFound from '../views/PageNotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)',
+      name: 'pageNotFound',
+      component: PageNotFound
     }
   ]
 })
