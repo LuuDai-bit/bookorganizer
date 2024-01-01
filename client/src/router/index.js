5a4f5519d7d7ee42aeff30f2c993d64b272e51d6
LuuDai-bit <jojolove198@gmail.com>
2024-01-01 16:54:49 +0700

    Books page and breadcrumbs

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../views/LoginView.vue'
 import Signup from '../views/SignupView.vue'
 import PageNotFound from '../views/PageNotFoundView.vue'
 import Verify from '../views/VerifyView.vue'
+import Book from '../views/BookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/books',
+      name: 'books',
+      component: Book,
     },
     {
       path: '/:pathMatch(.*)',
---
+++ client/src/views/BookView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <Breadcrumb paths="Books" />
+  </div>
+</template>
+
+<script>
+import Breadcrumb from '@/components/Breadcrumb.vue';
+
+export default {
+  name: "BookView",
+  components: {
+    Breadcrumb,
+  },
+  beforeMounted() {
+    // TODO: get list books
+  }
+}
+</script>
