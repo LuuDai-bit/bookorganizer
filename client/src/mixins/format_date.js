03767a6ea1cd4f7767f91ffc50b61dcef1a82c4a
LuuDai-bit <jojolove198@gmail.com>
2024-01-02 17:32:51 +0700

    Temporary fix zero date when create book

--- client/src/mixins/format_date.js
+++ client/src/mixins/format_date.js
@@ -3,6 +3,8 @@ import dayjs from "dayjs";
 const formatDate = (dateString) => {
   const date = dayjs(dateString);
 
+  if(dateString == '0001-01-01T00:00:00Z') return null;
+
   return date.format('MMMM D, YYYY');
 }
 
