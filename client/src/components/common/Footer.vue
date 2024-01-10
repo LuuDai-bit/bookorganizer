1feb3194415a64ac5f1efbafcb9f984316ba029e
LuuDai-bit <jojolove198@gmail.com>
2024-01-10 10:38:03 +0700

    Fix page desc

--- client/src/components/common/Footer.vue
+++ client/src/components/common/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-5"></div>
   <div class="w-full border-t-2 px-8 py-6 lg:flex justify-between items-center mt-auto bottom-0 h-5 relative max-width-inherit">
-    <p class="mb-2 lg:mb-0">© Copyright 2020</p>
+    <p class="mb-2 lg:mb-0">© Copyright 2024</p>
 
     <div class="flex">
       <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
